36c2c9da40317067bb52cf6b6fa6347f2c4aea80
Andrey Yudin <yudin_ag@ursip.ru>
2023-06-17 09:59:56 +0300

    .

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,28 @@
 <template>
-  <div><div>
-
-  </div></div>
+  <div>
+    <div>
+      <button @click="addLike">Like</button>
+      <button @click="addDislike">Dislike</button>
+    </div>
+    <div>Количество лайков: <strong>{{ likes }}</strong></div>
+    <div>Количество дизлайков: <strong>{{ dislikes }}</strong></div>
+  </div>
 </template>
 
 <script lang="ts">
 export default {
   data() {
     return {
-      likes: 0
+      likes: 0,
+      dislikes: 0,
+    }
+  },
+  methods: {
+    addLike() {
+      this.likes += 1
+    },
+    addDislike() {
+      this.dislikes += 1
     }
   }
 }

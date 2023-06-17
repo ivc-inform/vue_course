2a39807d6ca9d612569f74bade322795613e4ad5
Andrey Yudin <yudin_ag@ursip.ru>
2023-06-17 16:27:57 +0300

    .

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>
+  <div class="app">
+    <form action="">
+      <h4>Создание поста</h4>
+      <input class="input" type="text" placeholder="Названиие">
+      <input class="input" type="text" placeholder="Описание">
+      <button class="btn">Создать</button>
+    </form>
     <div class="post" v-for="post in posts">
       <div><strong>Название: </strong>{{ post.title }}</div>
       <div><strong>Описание: </strong>{{ post.body }}</div>
@@ -18,9 +24,7 @@ export default {
       ]
     }
   },
-  methods: {
-
-  }
+  methods: {}
 }
 </script>
 
@@ -35,5 +39,29 @@ export default {
   padding: 15px;
   border: 2px solid teal;
   margin-top: 15px;
+}
+
+.input {
+  width: 100%;
+  border: 1px solid teal;
+  padding: 15px;
+  margin-top: 15px;
+}
+
+.app {
+  padding: 20px;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+.btn {
+  align-self: flex-end;
+  margin-top: 15px;
+  padding: 10px 15px;
+  background: none;
+  border: 1px solid teal;
 }
 </style>
